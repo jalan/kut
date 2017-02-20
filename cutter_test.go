2f122629c1c4b40736b4f69236ef2821a811f969
jalan <jalanpalmer@gmail.com>
2017-02-20 18:30:34 -0500

    Convert to table-driven tests

--- cutter_test.go
+++ cutter_test.go
@@ -12,83 +12,36 @@ func TestNewCutter(t *testing.T) {
 	}
 }
 
-var testScanInput string = `aaa,bbb,ccc
-123,456,789`
+var scanTests = []struct {
+	input  string
+	ranges []ColRange
+	err    error
+	output string
+}{
+	{"", []ColRange{}, io.EOF, ""},
+	{"\n", []ColRange{}, io.EOF, ""},
+	{"abc", []ColRange{{1, 1}}, nil, "abc\n"},
+	{"abc,def,ghi", []ColRange{{1, 1}, {3, 3}}, nil, "abc,ghi\n"},
+	{"abc,def,ghi", []ColRange{{1, EOL}}, nil, "abc,def,ghi\n"},
+	{"abc,def,ghi", []ColRange{{2, 2}}, nil, "def\n"},
+	{"abc,def,ghi", []ColRange{{2, 3}}, nil, "def,ghi\n"},
+	{"abc,def,ghi", []ColRange{{3, 2}}, nil, "\n"},
+	{"abc,def,ghi", []ColRange{{3, EOL}}, nil, "ghi\n"},
+	{"abc,def,ghi", []ColRange{{4, EOL}}, nil, "\n"},
+	{"abc,def,ghi", []ColRange{}, nil, "\n"},
+}
 
 func TestScan(t *testing.T) {
-	i := bytes.NewBufferString(testScanInput)
-	o := new(bytes.Buffer)
-	c := NewCutter(i, o)
-	err := c.Scan()
-	if err != nil {
-		t.Errorf("Scan returned an error on a valid record")
-	}
-}
+	for _, test := range scanTests {
+		outBuf := new(bytes.Buffer)
+		c := NewCutter(bytes.NewBufferString(test.input), outBuf)
+		c.Ranges = test.ranges
+		if err := c.Scan(); err != test.err {
+			t.Errorf("expected return value %#v but got %#v", test.err, err)
+		}
+		if outString := outBuf.String(); outString != test.output {
+			t.Errorf("expected output %#v but got %#v", test.output, outString)
 
-func TestScanEOF(t *testing.T) {
-	i := bytes.NewBufferString(testScanInput)
-	o := new(bytes.Buffer)
-	c := NewCutter(i, o)
-	var err error
-	for err == nil {
-		err = c.Scan()
-	}
-	if err != io.EOF {
-		t.Errorf("Scan should eventually return io.EOF")
-	}
-}
-
-func TestScanNoRanges(t *testing.T) {
-	i := bytes.NewBufferString(testScanInput)
-	o := new(bytes.Buffer)
-	c := NewCutter(i, o)
-	c.Scan()
-	if o.String() != "\n" {
-		t.Errorf("Scan with nil Ranges should output one empty record")
-	}
-}
-
-func TestScanFullRange(t *testing.T) {
-	i := bytes.NewBufferString(testScanInput)
-	o := new(bytes.Buffer)
-	c := NewCutter(i, o)
-	c.Ranges = append(c.Ranges, ColRange{1, EOL})
-	c.Scan()
-	if o.String() != "aaa,bbb,ccc\n" {
-		t.Errorf("Scan with Ranges [{1, EOF}] should output one full record")
-	}
-}
-
-func TestScanOneColumn(t *testing.T) {
-	i := bytes.NewBufferString(testScanInput)
-	o := new(bytes.Buffer)
-	c := NewCutter(i, o)
-	c.Ranges = append(c.Ranges, ColRange{2, 2})
-	c.Scan()
-	if o.String() != "bbb\n" {
-		t.Errorf("Scan with Ranges %v should output one column", c.Ranges)
-	}
-}
-
-func TestScanTwoColumns(t *testing.T) {
-	i := bytes.NewBufferString(testScanInput)
-	o := new(bytes.Buffer)
-	c := NewCutter(i, o)
-	c.Ranges = append(c.Ranges, ColRange{2, 3})
-	c.Scan()
-	if o.String() != "bbb,ccc\n" {
-		t.Errorf("Scan with Ranges %v should output two columns", c.Ranges)
-	}
-}
-
-func TestScanMultipleRanges(t *testing.T) {
-	i := bytes.NewBufferString(testScanInput)
-	o := new(bytes.Buffer)
-	c := NewCutter(i, o)
-	c.Ranges = append(c.Ranges, ColRange{1, 1})
-	c.Ranges = append(c.Ranges, ColRange{3, 3})
-	c.Scan()
-	if o.String() != "aaa,ccc\n" {
-		t.Errorf("Scan with Ranges %v should output two columns", c.Ranges)
+		}
 	}
 }
